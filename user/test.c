cdd0b56fcc95ea12ec384dcebe40d016d4dd64eb
kobe-nunu <81320262+kobe-nunu@users.noreply.github.com>
2022-04-25 19:05:27 +0300

    Add files via upload

--- user/test.c
+++ user/test.c
@@ -9,55 +9,6 @@
 #include "kernel/memlayout.h"
 #include "kernel/riscv.h"
 
-void test_kill() {
-    int n_forks = 4;
-    int pid;
-    int k;
-    for (int i = 0; i < n_forks; i++) {
-    	pid = fork();
-        if(pid == 0)
-        {
-        k = i;
-        break;
-        }
-    }
-    if(pid==0){
-        while(1){
-            printf("this is process %d\n", k);
-            sleep(4);
-        }
-    }
-    else{
-        sleep(12);
-        killsystem();
-    }
-}
-
-void test_pause(int seconds) {
-    int n_forks = 4;
-    int pid;
-    int k;
-    for (int i = 0; i < n_forks; i++) {
-    	pid = fork();
-        if(pid == 0)
-        {
-        k = i;
-        break;
-        }
-    }
-    if(pid==0){
-        while(1){
-            printf("this is process %d\n", k);
-            sleep(4);
-        }
-    }
-    else{
-        sleep(12);
-        pausesystem(seconds);
-    }
-}
-
-
 
 void pause_system_dem(int interval, int pause_seconds, int loop_size) {
     int pid = getpid();
@@ -85,7 +36,7 @@ void kill_system_dem(int interval, int loop_size) {
     printf("\n");
 }
 
-void check(){
+void Scheduling_validator(){
     int n_forks = 4;
     int pid;
     int k;
@@ -104,28 +55,51 @@ void check(){
     }
 }
 
+void env(int size, int interval, char* env_name) {
+    int result = 1;
+    int loop_size = 10e6;
+    int n_forks = 2;
+    int pid;
+    for (int i = 0; i < n_forks; i++) {
+       pid = fork();
+    }
+    for (int i = 0; i < loop_size; i++) {
+        if (i % (loop_size / (int) 10e0) == 0) {
+        	if (pid == 0) {
+        		printf("%s %d/%d completed.\n", env_name, i, loop_size);
+        	} else {
+        		printf(" ");
+        	}
+        }
+        if (i % interval == 0) {
+            result = result * size;
+        }
+    }
+    printf("\n");
+  
+}
 
-// void set_economic_mode_dem(int interval, int loop_size) {
-//     int pid = getpid();
-//     set_economic_mode(1);
-//     for (int i = 0; i < loop_size; i++) {
-//         if (i % interval == 0 && pid == getpid()) {
-//             printf("set economic mode %d/%d completed.\n", i, loop_size);
-//         }
-//         if (i == loop_size / 2) {
-//             set_economic_mode(0);
-//         }
-//     }
-//     printf("\n");
-// }
+void env_large() {
+    env(10e6, 10e6, "env_large");
+}
+
+void env_freq() {
+    env(10e1, 10e1, "env_freq");
+}
+
+void PauseKillTest(int a, int b, int c, int d, int e){
+    pause_system_dem(a,b,c);
+    kill_system_dem(d,e);
+}
 
 int
 main(int argc, char *argv[])
 {
-    // set_economic_mode_dem(10, 100);
-    check();
-    exit(0);
-    pause_system_dem(10, 10, 100);
-    kill_system_dem(10, 100);
+    // PauseKillTest(10,3,100,10,100);
+    // Scheduling_validator();
+    // test_pause();
+    // env_large();
+    env_freq();
+    print_stats();
     exit(0);
 }
